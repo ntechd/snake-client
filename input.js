c7ef00ee97ebe030e5a42535fee40f6a31a933a7
Najma Duale <najmaduale1@gmail.com>
2021-12-05 03:38:18 +0000

    adding files

---
+++ input.js
@@ -0,0 +1,27 @@
+const handleUserInput = function(key) {
+  if(key==='\u0003')
+    process.exit();
+  if(key==='w')
+    connection.write("Move: up");
+  if(key==='a')
+    connection.write("Move: left");
+  if(key==='s')
+    connection.write("Move: down");
+  if(key==='d')
+    connection.write("Move: right");
+  if(key==='m')
+    connection.write("Say: I m the winner!!!");
+
+};
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data",handleUserInput);
+  return stdin;
+};
+
+module.exports = {setupInput};
